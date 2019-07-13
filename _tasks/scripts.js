7a1852c306a1ff073f69ff7d886e8cec69d35ab5
Apurv <apurvmishra99@gmail.com>
2019-07-13 13:21:33 +0100

    fix svg files and other changes

--- _tasks/scripts.js
+++ _tasks/scripts.js
@@ -1,45 +1,43 @@
-
 const config = require('./_config.json')
 const gulp = require('gulp')
 const rename = require('gulp-rename')
 const uglify = require('gulp-uglify')
-const webpack = require('webpack')
-const webpackStream = require('webpack-stream')
-require('dotenv').config()
+const webpack = require('webpack-stream')
 
-const babelConfig = {
-    test: /\.m?js$/,
-    exclude: /(node_modules|bower_components)/,
-    use: {
-        loader: 'babel-loader',
-        options: {
-            presets: ['@babel/preset-env'],
-            plugins: [['@babel/plugin-transform-react-jsx', { pragma: 'h' }]]
-        }
-    }
-}
 const webpackConfig = {
     entry: {
         main: `./${config.assetSrc}/scripts/main.js`,
         webmentions: `./${config.assetSrc}/scripts/webmentions/index.js`
     },
-    output: { filename: '[name].js' },
-    module: {
-        rules: [babelConfig]
+    output: {
+        filename: '[name].js'
     },
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: [
+                            [
+                                '@babel/plugin-transform-react-jsx',
+                                { pragma: 'h' }
+                            ]
+                        ]
+                    }
+                }
             }
-        })
-    ]
+        ]
+    }
 }
 
-gulp.task('scripts', function() {
+gulp.task('scripts', function () {
     return gulp
         .src(config.assetSrc + '/scripts/main.js')
-        .pipe(webpackStream(webpackConfig))
+        .pipe(webpack(webpackConfig))
         .pipe(uglify())
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest(config.assetDest + '/js'))
